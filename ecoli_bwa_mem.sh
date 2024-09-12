b38c4d1da2726160135f81993a3263dc75a7573c
tanman6711 <111311691+tanman6711@users.noreply.github.com>
2024-09-12 16:41:45 -0500

    Adding task 6 files

---
+++ ecoli_bwa_mem.sh
@@ -0,0 +1,1 @@
+bwa mem -t 4 /scratch/biol726307/BIOL7263_Genomics/reference_sequences/ecoli/GCF_000005845.2_ASM584v2_genomic /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/trimmed_reads_val_1.fq.gz /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/trimmed_reads_val_2.fq.gz -o /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/mapping_to_reference/ecoli_mapped.sam
