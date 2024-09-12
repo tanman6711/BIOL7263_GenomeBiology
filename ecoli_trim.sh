71911da49873f19088a65ad75527827b5ac96aa1
tanman6711 <111311691+tanman6711@users.noreply.github.com>
2024-09-12 15:58:36 -0500

    Add task 2 files

---
+++ ecoli_trim.sh
@@ -0,0 +1,1 @@
+trim_galore --paired --fastqc --gzip --cores 4 --length 100 /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/read_1.fastq.gz /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/read_2.fastq.gz --basename trimmed_reads -o /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/
