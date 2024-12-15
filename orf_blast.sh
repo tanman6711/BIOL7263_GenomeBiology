f4e4d07f29a2495f479a6aa2705986c830e1d4f4
tanman6711 <111311691+tanman6711@users.noreply.github.com>
2024-12-15 16:05:28 -0600

    Adding more ch 4 files

---
+++ orf_blast.sh
@@ -0,0 +1,1 @@
+blastp -query /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/assembly/orfs.fa -db /scratch/biol726307/BIOL7263_Genomics/db/Ec_prot -outfmt "6 qseqid sseqid pident stitle" -max_target_seqs 1 | sort -u > /scratch/biol726307/BIOL7263_Genomics/sequencing_data/ecoli/assembly/orf_best_hit.txt
